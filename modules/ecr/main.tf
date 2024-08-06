42646dca23787dd48c191c6d85b46bd94ee58679
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-08-06 18:56:16 +0900

    Add ECR for proxy service

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -12,3 +12,18 @@ resource "aws_ecr_repository" "frontend" {
     Name    = "${var.project}-${var.env}-frontend-ecr-repo"
   }
 }
+
+resource "aws_ecr_repository" "proxy" {
+  name                 = "${var.project}-${var.env}-proxy-ecr-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${var.project}-${var.env}-proxy-ecr-repo"
+  }
+}
