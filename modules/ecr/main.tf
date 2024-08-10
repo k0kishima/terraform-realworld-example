d3c6523b0c108194a92b9bbb4957e889c6966985
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-08-10 14:22:52 +0900

    Add ECR for backend app

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,3 +1,18 @@
+resource "aws_ecr_repository" "proxy" {
+  name                 = "${var.project}-${var.env}-proxy-ecr-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${var.project}-${var.env}-proxy-ecr-repo"
+  }
+}
+
 resource "aws_ecr_repository" "frontend" {
   name                 = "${var.project}-${var.env}-frontend-ecr-repo"
   image_tag_mutability = "MUTABLE"
@@ -13,8 +28,8 @@ resource "aws_ecr_repository" "frontend" {
   }
 }
 
-resource "aws_ecr_repository" "proxy" {
-  name                 = "${var.project}-${var.env}-proxy-ecr-repo"
+resource "aws_ecr_repository" "backend" {
+  name                 = "${var.project}-${var.env}-backend-ecr-repo"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -24,6 +39,43 @@ resource "aws_ecr_repository" "proxy" {
   tags = {
     Env     = var.env
     Project = var.project
-    Name    = "${var.project}-${var.env}-proxy-ecr-repo"
+    Name    = "${var.project}-${var.env}-backend-ecr-repo"
   }
+}
+
+locals {
+  lifecycle_policy = {
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only the last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "proxy" {
+  repository = aws_ecr_repository.proxy.name
+
+  policy = jsonencode(local.lifecycle_policy)
+}
+
+resource "aws_ecr_lifecycle_policy" "frontend" {
+  repository = aws_ecr_repository.frontend.name
+
+  policy = jsonencode(local.lifecycle_policy)
+}
+
+resource "aws_ecr_lifecycle_policy" "backend" {
+  repository = aws_ecr_repository.backend.name
+
+  policy = jsonencode(local.lifecycle_policy)
 }
